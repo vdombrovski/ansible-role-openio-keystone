7789a8dc80f4efc51eb45a3890634a10c197c3e9
Delgehier Cédric <cedric.delgehier@gmail.com>
2018-04-19 15:50:52 +0000

    Add CI

--- docker-tests.sh
+++ docker-tests.sh
@@ -4,7 +4,9 @@
 #
 # Runs tests for this Ansible role on a Docker container
 # Environment variables DISTRIBUTION and VERSION must be set
-# See usage() for details.
+# outside of the script, e.g.
+#
+# $ DISTRIBUTION=centos VERSION=7 docker-tests.sh
 
 #{{{ Bash settings
 # abort on nonzero exitstatus
@@ -20,13 +22,19 @@ readonly script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 IFS=$'\t\n'   # Split on newlines and tabs (but not on spaces)
 
 readonly container_id="$(mktemp)"
-readonly role_dir='/etc/ansible/roles/openio-keystone'
-readonly test_playbook="${role_dir}/tests/test.yml"
-readonly inventory="${role_dir}/tests/inventory"
-readonly requirements="${role_dir}/tests/requirements.yml"
+readonly role_dir='/etc/ansible/roles/role_under_test'
+if [ "$#" -ne 1 ]; then
+    readonly test_playbook="${role_dir}/docker-tests/test.yml"
+else
+    readonly test_playbook="${role_dir}/docker-tests/$1.yml"
+fi
+readonly requirements="${role_dir}/docker-tests/requirements.yml"
 
 #readonly docker_image="bertvv/ansible-testing"
-readonly docker_image="testkeystone"
+#readonly image_tag="${docker_image}:${DISTRIBUTION}_${VERSION}"
+
+readonly docker_image="cdelgehier/docker_images_ansible"
+readonly image_tag="${docker_image}:${ANSIBLE_VERSION}_${DISTRIBUTION}_${VERSION}"
 
 # Distribution specific settings
 init="/sbin/init"
@@ -34,54 +42,73 @@ run_opts=("--privileged")
 #}}}
 
 main() {
-  configure_env
+  configure_environment
 
   start_container
 
   run_galaxy_install
   run_syntax_check
-  run_playbook
+  run_test_playbook
   run_idempotence_test
 
+  # Uncomment the following line if you want to clean up the
+  # container(s) after running the tests. *Not* cleaning up may be
+  # useful for troubleshooting
+
+  # cleanup
 }
 
 #{{{ Helper functions
 
-configure_env() {
+# Usage: configure_environment.
+#
+# This function defines run options for Docker, depending on the
+# Linux $DISTRIBUTION and $VERSION.
+configure_environment() {
 
   case "${DISTRIBUTION}_${VERSION}" in
-    'centos_7')
+    'centos_6')
+      run_opts+=('--volume=/sys/fs/cgroup:/sys/fs/cgroup:ro')
+      ;;
+    'centos_7'|'fedora_25')
       init=/usr/lib/systemd/systemd
       run_opts+=('--volume=/sys/fs/cgroup:/sys/fs/cgroup:ro')
       ;;
     'ubuntu_14.04')
+      #run_opts+=('--volume=/sys/fs/cgroup:/sys/fs/cgroup:ro')
       # Workaround for issue when the host operating system has SELinux
       if [ -x '/usr/sbin/getenforce' ]; then
         run_opts+=('--volume=/sys/fs/selinux:/sys/fs/selinux:ro')
       fi
       ;;
-    'ubuntu_16.04')
+    'ubuntu_16.04'|'debian_8')
       run_opts=('--volume=/run' '--volume=/run/lock' '--volume=/tmp' '--volume=/sys/fs/cgroup:/sys/fs/cgroup:ro' '--cap-add=SYS_ADMIN' '--cap-add=SYS_RESOURCE')
 
-#      if [ -x '/usr/sbin/getenforce' ]; then
-#        run_opts+=('--volume=/sys/fs/selinux:/sys/fs/selinux:ro')
-#      fi
+      #if [ -x '/usr/sbin/getenforce' ]; then
+      #  run_opts+=('--volume=/sys/fs/selinux:/sys/fs/selinux:ro')
+      #fi
+      ;;
+    *)
+      log "Warning: no run options added for ${DISTRIBUTION} ${VERSION}"
       ;;
   esac
 }
 
 # Usage: build_container
 build_container() {
-  docker build --tag="${docker_image}:${DISTRIBUTION}_${VERSION}" .
+  log "Building container for ${DISTRIBUTION} ${VERSION}"
+  set -x
+  docker build --tag="${image_tag}" .
+  set +x
 }
 
 start_container() {
   log "Starting container"
   set -x
   docker run --detach \
-    --volume="${PWD}:${role_dir}:ro" \
     "${run_opts[@]}" \
-    "${docker_image}:${DISTRIBUTION}_${VERSION}" \
+    --volume="${PWD}:${role_dir}:ro" \
+    "${image_tag}" \
     "${init}" \
     > "${container_id}"
   set +x
@@ -92,15 +119,22 @@ get_container_id() {
 }
 
 # Usage: get_container_ip CONTAINER_ID
+#
+# Prints the IP address of the specified container.
 get_container_ip() {
-  local container_id="${1}"
+  local id
+  id="$(get_docker_id)"
 
   docker inspect \
     --format '{{ .NetworkSettings.IPAddress }}' \
-    "${container_id}"
+    "${id}"
 }
 
+# Usage: exec_container COMMAND
+#
+# Run COMMAND on the Docker container
 exec_container() {
+  local id
   id="$(get_container_id)"
 
   set -x
@@ -112,28 +146,29 @@ exec_container() {
 }
 
 run_syntax_check() {
-  log "Running syntax check on playbook"
-  exec_container ansible-playbook -i "${inventory}" "${test_playbook}" --syntax-check
+  log 'Running syntax check on playbook'
+  exec_container ansible-playbook "${test_playbook}" --syntax-check
+  log 'Syntax check finished'
 }
 
-run_playbook() {
-  log "Running playbook"
-  exec_container ansible-playbook -i "${inventory}" "${test_playbook}" --diff
-  log "Run finished"
+run_test_playbook() {
+  log 'Running playbook'
+  exec_container ansible-playbook "${test_playbook}" --diff 
+  log 'Run finished'
 }
 
 run_galaxy_install() {
   log "Running ansible-galaxy install"
   exec_container ansible-galaxy install -r "${requirements}"
-  log "Run finished"
+  log "Requirements installed"
 }
 
 run_idempotence_test() {
-  log "Running idempotence test"
+  log 'Running idempotence test' 
   local output
   output="$(mktemp)"
 
-  exec_container ansible-playbook -i "${inventory}" "${test_playbook}" 2>&1 | tee "${output}"
+  exec_container ansible-playbook "${test_playbook}" --diff 2>&1 | tee "${output}"
 
   if grep -q 'changed=0.*failed=0' "${output}"; then
     result='pass'
@@ -149,34 +184,13 @@ run_idempotence_test() {
 }
 
 cleanup() {
-  log "Cleaning up"
+  log 'Cleaning up'
+  local id
   id="$(get_container_id)"
 
   docker stop "${id}"
   docker rm "${id}"
   rm "${container_id}"
-}
-
-# Check if command line arguments are valid
-check_args() {
-  if [ "${#}" -ne "1" ]; then
-    echo "Expected 1 argument, got ${#}" >&2
-    usage
-    exit 2
-  fi
-}
-
-# Print usage message on stdout
-usage() {
-cat << _EOF_
-Usage: ${script_name} [OPTIONS]... [ARGS]...
-
-  Creates a Docker container, installs this role, and runs tests.
-
-OPTIONS:
-
-EXAMPLES:
-_EOF_
 }
 
 log() {
@@ -190,4 +204,3 @@ log() {
 
 main "${@}"
 
-#trap cleanup EXIT INT ERR HUP TERM
